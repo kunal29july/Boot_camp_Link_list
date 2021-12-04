7ca982567362f4fcc939b71247403cb36123599a
Kunal Mahajan <78916226+kunal29july@users.noreply.github.com>
2021-12-04 21:43:23 +0530

    Create Merge Two Sorted Lists.py

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,57 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+'''
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1=list1
+        curr2=list2
+        node=ListNode(0)
+        head=node
+        
+        while(curr1 and curr2):
+            if curr1.val<=curr2.val:
+                
+                node.next=ListNode(curr1.val)
+                curr1=curr1.next
+                node=node.next
+            else:
+                node.next=ListNode(curr2.val)
+                curr2=curr2.next
+                node=node.next
+                
+        while(curr1):
+            node.next=ListNode(curr1.val)
+            node=node.next
+            curr1=curr1.next
+        while(curr2):
+            node.next=ListNode(curr2.val)
+            curr2=curr2.next
+            node=node.next
+        return head.next
